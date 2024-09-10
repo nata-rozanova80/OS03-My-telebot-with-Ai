be95261a7ae30391e22a20853161e76663f9d694
nata-rozanova80 <nata.rozanova80@gmail.com>
2024-09-10 19:05:30 +0300

    Add main and readme

---
+++ main.py
@@ -0,0 +1,41 @@
+# My_toking_bot
+import telebot
+from openai import OpenAI
+
+# Инициализация клиента API OpenAI с вашим API ключом
+client = OpenAI(
+    api_key="sk-eojihWMYuwlwO4oNjNMX8DbkkkBtLg7I",
+    base_url="https://api.proxyapi.ru/openai/v1",
+)
+
+# Список для хранения истории разговора
+conversation_history = []
+
+# Инициализация Telegram бота
+bot = telebot.TeleBot("6613597709:AAEKNx2I74OyAL77BQj-_FuA9w6VuzPl120")
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    # Получение текстового сообщения от пользователя
+    user_input = message.text
+
+    # Добавление ввода пользователя в историю разговора
+    conversation_history.append({"role": "user", "content": user_input})
+
+    try:
+        # Отправка запроса в нейронную сеть
+        chat_completion = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=conversation_history
+        )
+
+        # Извлечение и вывод ответа нейронной сети
+        ai_response_content = chat_completion.choices[0].message.content
+        bot.reply_to(message, "AI: " + ai_response_content)
+
+        # Добавление ответа нейронной сети в историю разговора
+        conversation_history.append({"role": "assistant", "content": ai_response_content})
+
+    except Exception as e:
+        bot.reply_to(message, "Произошла ошибка: " + str(e))
+
+bot.polling()
